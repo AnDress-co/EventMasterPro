e820ff98274a5d17c0ebc5849cfb403f6fd07930
AnDress-co <gomezmarlon872@gmail.com>
2025-04-16 18:12:22 -0500

    initial commit

---
+++ src/main/java/com/mycompany/eventmasterpro/Main.java
@@ -0,0 +1,13 @@
+package com.mycompany.eventmasterpro;
+
+/**
+ *
+ * @author Marlon Gomez, Yermy Sotelo, Sergio Motato
+ * 
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
